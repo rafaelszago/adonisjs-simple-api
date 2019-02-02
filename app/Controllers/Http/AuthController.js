be29660a8778e609fb2b10733e6f34a80bc35b4b
Rafael Zago <rafael.zago@br-mediagroup.com>
2019-02-02 15:28:52 -0200

    :sparkles: Add register route

--- app/Controllers/Http/AuthController.js
+++ app/Controllers/Http/AuthController.js
@@ -1,11 +1,15 @@
 'use strict'
 
-const User = user('App/Models/User')
+const User = use('App/Models/User')
 
 class AuthController {
   async register({ request }) {
     const data = request.only(['username', 'email', 'password'])
+
     const user = await User.create(data)
+
     return user
   }
 }
+
+module.exports = AuthController
